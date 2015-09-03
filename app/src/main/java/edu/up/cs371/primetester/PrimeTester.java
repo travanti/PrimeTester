2671899e5231411c5089a6df8571eb5e4dd8c2e2
travan <nathantravanti@gmail.com>
2015-09-02 17:17:27 -0700

    Fixed isPrime with sensible code

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -15,15 +15,19 @@ public class PrimeTester {
      */
     public static boolean isPrime(long n)
     {
-        if(n==2) return true;
-        //check if n is a multiple of 2
-        if(n%2==0) return false;
-        //create a loop to count through possible numbers
-        for(int i=3;i*i<n;i+=2)
+        int result = 0;
+        for(int i=2;i<=n/2;i++)
         {
-            if (n % 1 == 0)
-                return false;
+            if (n % i == 0)
+                result = 1;
         }
-        return true;
+        if(result ==1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }
